0ec4264b34f2bc9d99c4e617516d8d4f09664b29
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2023-11-30 20:27:07 +0330

    fix rendering index.html

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"github.com/jafari-mohammad-reza/echo-htmx-practice/src/pkg"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
+	"net/http"
+)
 
 type Msg struct {
 	Message string
@@ -8,17 +14,17 @@ type Msg struct {
 
 func main() {
 	e := echo.New()
-
+	e.Debug = true
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Recover())
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(
 		rate.Limit(20),
 	)))
 
-	template.NewTemplateRenderer(e, "public/*.html")
+	pkg.NewTemplateRenderer(e, "views/*.html")
 	e.GET("/hello", func(e echo.Context) error {
 		msg := Msg{Message: "Hello world"}
-		return c.Render(http.StatusOK, "index", msg)
+		return e.Render(http.StatusOK, "index.html", msg)
 	})
 
 	e.Logger.Fatal(e.Start(":4040"))
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 build:
-	@go build -o ./dist/api src/main.go
+	@go build -o ./dist/api main.go
 run: build
 	@./dist/api
 dev:
-	@/go/bin/reflex -s -r '\.go' -R '^vendor/.' -R '^_.*' go run src/main.go
+	@~/go/bin/reflex -s -r '\.go' -R '^vendor/.' -R '^_.*' go run main.go
 docker-dev:
 	@docker rm -f finfetch || true && docker build -t finfetch . && docker run --name finfetch -v $$(pwd):/app -p 5050:5050  finfetch
